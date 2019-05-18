388cd4fe2e12e5e2425cca92d964f38adfbee6c4
Andy Palmer <atp@sdf.org>
2019-05-17 23:44:24 -0500

    Play middle C

--- miditool.c
+++ miditool.c
@@ -47,11 +47,24 @@ size_t fill_header(char *out, twobytes format, twobytes tracks, twobytes divisio
 }
 
 
+size_t fill_midi_event(char *out) {
+    const fourbytes size = utofourbytes(4);
+    memcpy(&out[0], MIDI_TRACK, 4);
+    memcpy(&out[4], &size, 4);
+    out[8] = 0; /* deltatime */
+    out[9] = 0x09 << 4 | 0x00; /* "note on" status (0x09) | MIDI channel (0x00) */
+    out[10] = 0x3C; /* middle C */
+    out[11] = 0x7F; /* max velocity */
+    return 12;
+}
+
+
 int main(int argc, char **argv) {
     size_t bytesused = 0;
     char buff[1024] = {0};
 
-    bytesused += fill_header(&buff[0], utotwobytes(0), utotwobytes(0), utotwobytes(0));
+    bytesused += fill_header(&buff[bytesused], utotwobytes(0), utotwobytes(0), utotwobytes(0));
+    bytesused += fill_midi_event(&buff[bytesused]);
 
     FILE *f = fopen("out.mid", "wb");
     fwrite(&buff, bytesused, 1, f);
