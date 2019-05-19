b8f4251974ec8908b1b60b03baf6cc9550a4906d
Andrew Palmer <atp@sdf.org>
2019-05-19 01:34:26 -0500

    Create track helper functions

--- miditool.c
+++ miditool.c
@@ -68,9 +68,22 @@ size_t fill_header(char *out, uint16_t format, uint16_t tracks, uint16_t divisio
 }
 
 
-size_t track_midi_event(char *track, char delta, char status, char channel, char pitch, char velocity) {
+char *track_pointer(char *track) {
     fourbytes curr_bytes = flip4(*(uint32_t *)&track[4]);
     char *p = &track[8 + curr_bytes.value];
+    return p;
+}
+
+
+uint32_t track_update(char *track, uint32_t new_bytes) {
+    fourbytes curr_bytes = flip4(*(uint32_t *)&track[4]);
+    *(fourbytes *)&track[4] = flip4(curr_bytes.value + new_bytes);
+    return new_bytes;
+}
+
+
+size_t track_midi_event(char *track, char delta, char status, char channel, char pitch, char velocity) {
+    char *p = track_pointer(track);
 
     uint32_t new_bytes = 0;
     if(delta)
@@ -80,9 +93,7 @@ size_t track_midi_event(char *track, char delta, char status, char channel, char
     p[new_bytes++] = pitch;
     p[new_bytes++] = velocity;
 
-    *(fourbytes *)&track[4] = flip4(curr_bytes.value + new_bytes); /* update track size */
-
-    return new_bytes;
+    return track_update(track, new_bytes);
 }
 
 
@@ -94,16 +105,13 @@ size_t track_init(char *out) {
 
 
 size_t track_end(char *track) {
-    fourbytes curr_bytes = flip4(*(uint32_t *)&track[4]);
-    char *p = &track[8 + curr_bytes.value];
+    char *p = track_pointer(track);
 
     p[0] = 0xFF;
     p[1] = 0x2F;
     p[2] = 0x00;
 
-    *(fourbytes *)&track[4] = flip4(curr_bytes.value + 3); /* update track size */
-
-    return 3;
+    return track_update(track, 3);
 }
 
 
